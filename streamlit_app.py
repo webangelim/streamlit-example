8256ca1f2247d1891dddbd02e36fb4291f8c5853
webangelim <99106831+webangelim@users.noreply.github.com>
2024-04-09 11:10:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 
 # ----- Interface ----- #
-# openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-openai_api_key = "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"
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+# openai_api_key = "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"
 
 st.title(':rainbow[InterrogaPPC-Inator]')
 
